b967c8b142073bdc1b07da5d5dbed884fa0ecd56
Frank Corrigan <franciscorrigan3@gmail.com>
2015-07-24 15:16:39 -0400

    Adding new Lesson 6 exercises.

---
+++ OpenStreetMap-Analysis/BoulderCO.py
@@ -0,0 +1,31 @@
+boulder_tags = { 'bounds': 1,
+				 'member': 25395,
+				 'nd': 3396203,
+				 'node': 2909855,
+				 'osm': 1,
+				 'relation': 1405,
+				 'tag': 1690297,
+				 'way': 316988 }
+
+
+dataModel = {
+	"_id" : "1234",
+	"visible" : "true"
+	"created" : {
+				 "version" : "2",
+				 "changeset" : "18234",
+				 "timestamp" : "2013-08-03T16:43:422",
+				 "user" : "linuxUser2",
+				 "uid" : "123123"
+				},
+	"pos" : [41.45,-87.2],
+	"address" : {
+				 "housenumber" : "3232",
+				 "postcode" : "232323",
+				 "street" : "Noth Lincol Ave."
+				 },
+	"amenity" : "restaurant",
+	"cuisine" : "mexican",
+	"name" : "La Mexico",
+	"phone" : "1 (564) 231-2333"
+}
