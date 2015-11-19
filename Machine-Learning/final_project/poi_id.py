0b76dd2d5feb1d9176454c8e39439fcf8a3753a6
FrankCorrigan <franciscorrigan3@gmail.com>
2015-11-19 13:13:02 -0500

    adding other files theh checkout decided to leave out

--- Machine-Learning/final_project/poi_id.py
+++ Machine-Learning/final_project/poi_id.py
@@ -10,8 +10,27 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi', 'total_payments', 'exercised_stock_options', 
-                 'bonus', 'expenses', 'poi_mail', 's_ratio'] # You will need to use more features
+# features_list = ['poi', 'bonus', 'salary', 'total_stock_value', 'deferred_income',
+#                  'short_gain', 'total_total', 'sal_ratio',
+#                  'shared_receipt_with_poi', 'restricted_stock', 'em_ratio', 'other'] # You will need to use more features
+
+
+# features_list = ['poi', 'bonus', 'salary', 'exercised_stock_options',
+#                  'short_gain', 'total_total', 'sal_ratio', 'from_ratio',
+#                  'shared_receipt_with_poi', 'restricted_stock', 'em_ratio', 'other'] 
+# 0.8695, 0.5682, 0.0895, 0.15 
+
+# features_list = ['poi', 'salary', 'bonus', 'total_stock_value', 'exercised_stock_options',
+#                  'from_ratio', 'short_gain', 'deferred_income', 'total_total']
+# Random Forest, Accuracy: 0.87147	Precision: 0.57725	Recall: 0.13450	F1: 0.21
+
+# features_list = ['poi', 'salary', 'bonus', 'total_stock_value', 'exercised_stock_options',
+#                  'from_ratio', 'short_gain', 'deferred_income', 'total_total', 'sal_ratio']
+# Random Forest, Accuracy: 0.87247	Precision: 0.59732	Recall: 0.13350	F1: 0.21
+
+features_list = ['poi', 'salary','shared_receipt_with_poi','deferred_income','bonusexpsal',
+ 'em_ratio','total_navalue_count','total_total', 'sal_navalue_count','fromto_ratio']
+
 print('features: ', features_list)
 
 ### Load the dictionary containing the dataset
@@ -24,22 +43,44 @@ del data_dict['TOTAL']
 ### Task 3: Create new feature(s)
 import pandas as pd
 import numpy as np
+
 pdata = pd.DataFrame.from_dict(data_dict, orient='index')
-pdata = pdata.replace("NaN", 0, regex=True)
-# I assume poi's have relatively greater email exchange with other pois
-# Create percentage of poi-related email to total emails for each individual
-pdata['poi_mail'] = ( pdata['from_poi_to_this_person'] + pdata['from_this_person_to_poi'] + pdata['shared_receipt_with_poi'] ) / ( pdata['to_messages'] + pdata['from_messages'] )
-# I assume that people receiving relatively greater incentive pay are receiving it for a reason
-# Create percentage of incentive pay to salary
-pdata['s_ratio'] = ( pdata['bonus'] ) / pdata['salary']
-# Need to replace Nan and inf values
-# Need to turn poi variable back into character from integer
+
+pdata = pdata.replace('NaN', np.nan, regex=True)
+
+temp = []
+for el in pdata['email_address'].values:
+    if type(el) == float:
+        temp.append(False)
+    else:
+        temp.append(True)
+pdata['has_email_data'] = temp
+pdata['total_navalue_count'] = pdata.isnull().sum(axis=1)
+pdata['sal_navalue_count'] = pdata[['salary', 'deferral_payments', 'long_term_incentive', 'total_payments', 'bonus', 'expenses', 'loan_advances', 'other', 'director_fees', 'deferred_income']].isnull().sum(axis=1)
+
+pdata = pdata.drop('email_address', 1)
+
+pdata = pdata.replace(np.nan, 0, regex=True)
+
+pdata = pdata[pdata.index != 'SKILLING JEFFREY K']
+pdata = pdata[pdata.index != 'BELFER ROBERT']
+pdata = pdata[pdata.index != 'BHATNAGAR SANJAY']
+
+pdata['short_gain'] = pdata['exercised_stock_options'] - pdata['long_term_incentive']
+pdata['sal_ratio'] = pdata['salary'] / pdata['total_payments']
+pdata['nonsal_ratio'] = (pdata['bonus'] + pdata['long_term_incentive'] + pdata['expenses']) / pdata['total_payments']
+pdata['stock_ratio'] = pdata['restricted_stock'] / pdata['total_stock_value']
+pdata['em_ratio'] = (pdata['shared_receipt_with_poi'] + pdata['from_this_person_to_poi'] + pdata['from_poi_to_this_person'] ) / (pdata['to_messages'] + pdata['from_messages'])
+pdata['total_total'] = pdata['total_payments'] + pdata['total_stock_value']
+pdata['from_ratio'] = pdata['from_this_person_to_poi'] / pdata['from_messages']
+pdata['sent_ratio'] = (pdata['from_poi_to_this_person'] + pdata['shared_receipt_with_poi']) / pdata['to_messages']
+pdata['fromto_ratio'] = pdata['from_ratio'] + pdata['sent_ratio']
+pdata['bonusexpsal'] = pdata['bonus'] + pdata['expenses'] + pdata['salary']
+
 pdata = pdata.replace(np.nan, 0, regex=True)
 pdata = pdata.replace(np.inf, 0, regex=True)
 pdata = pdata.replace(-np.inf, 0, regex=True)
 pdict = pdata.T.to_dict()
-
-print('Sample element of data', pdict.itervalues().next())
 
 ### Store to my_dataset for easy export below.
 my_dataset = pdict
@@ -48,8 +89,11 @@ my_dataset = pdict
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
 
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = \
+    train_test_split(features, labels, test_size=0.3, random_state=42)
+
 ### Scale the feature set
-from sklearn.preprocessing import MinMaxScaler
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -69,18 +113,16 @@ from sklearn.preprocessing import MinMaxScaler
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.1, random_state=42)
+# from sklearn.cross_validation import train_test_split
+# features_train, features_test, labels_train, labels_test = \
+#     train_test_split(features, labels, test_size=0.3, random_state=42)
 
-print('Test set created...')
-print('Building random forest...')
+# rfc = RandomForestClassifier(n_estimators=100,
+# 							criterion='entropy',
+# 							min_samples_split=25)
+# NOTE: Higher min_samples_split decreases accuracy.. but your
+# false positive rate falls sharply..
 
-from sklearn.pipeline import Pipeline
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.grid_search import GridSearchCV
-
-rfc = RandomForestClassifier(n_estimators=100)
 # param_grid = {"model__max_depth": [3, None],
 #               "model__max_features": [1, 3, 5],
 #               "model__min_samples_split": [1, 3, 10],
@@ -88,16 +130,63 @@ rfc = RandomForestClassifier(n_estimators=100)
 #               "model__bootstrap": [True, False],
 #               "model__criterion": ["gini", "entropy"]}
 
-scaler = MinMaxScaler()
-pipeline_object = Pipeline([('scaler', scaler),('model', rfc)])
-# clf = GridSearchCV(pipeline_object, param_grid, scoring = 'accuracy')
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
 
-clf = pipeline_object.fit(features_train, labels_train)
-print('This is the final classifier...')
+# pipe_svc = Pipeline([('scl', StandardScaler()),
+#                      ('pca', PCA(n_components=2)),
+#                      ('clf', SVC(random_state=1,
+#                      			 kernel='linear',
+#                      			 C=1))])
 
-from sklearn import datasets, linear_model, cross_validation, grid_search
-kf_total = cross_validation.KFold(len(features), n_folds=10, indices=True, shuffle=True, random_state=4)
-print([clf.fit(features_train, labels_train).score(features_test,labels_test) for train_indices, test_indices in kf_total])
+# pipe_rfc = Pipeline([('scl', MinMaxScaler()),
+#                      ('pca', PCA(n_components=2)),
+#                      ('clf', RandomForestClassifier(random_state=1,
+#                      			 n_estimators=100,
+#                      			 criterion='gini',
+#                      			 min_samples_split=20))])
+
+# pipe_rfc = Pipeline([('scl', StandardScaler()),
+#                      ('pca', PCA(n_components=2)),
+#                      ('clf', AdaBoostClassifier(RandomForestClassifier(
+#                                                 n_estimators=100,
+#                                                 criterion='gini',
+#                                                 min_samples_split=40,
+#                                                 random_state=1)))])
+
+# pipe_dtc = Pipeline([('scl', MinMaxScaler()),
+#                      ('pca', PCA(n_components=2)),
+#                      ('clf', DecisionTreeClassifier(criterion='gini',
+#                      								min_samples_split=10,
+#                      								random_state=1))])
+# features_list = ['poi', 'salary', 'total_payments', 'exercised_stock_options',
+# 				 'bonus','total_stock_value', 'deferred_income', 'from_ratio',
+#  				 'sal_ratio', 'total_total', 'short_gain']
+
+# pipe_dtc = Pipeline([('scl', MinMaxScaler()),
+#                      ('pca', PCA(n_components=2)),
+#                      ('clf', DecisionTreeClassifier(criterion='entropy',
+#                                                   min_samples_split=20,
+#                                                   random_state=1))])
+
+pipe_dtc = Pipeline([('scl', MinMaxScaler()),
+                     ('pca', PCA(n_components=2)),
+                     ('clf', DecisionTreeClassifier(criterion='gini',
+                                                  min_samples_split=40,
+                                                  random_state=1))])
+# features_list = ['poi', 'salary','shared_receipt_with_poi','deferred_income','bonusexp',
+#  'nonsal_ratio','em_ratio','total_navalue_count','total_total','sal_navalue_count','fromto_ratio']
+# Accuracy: 0.89133   Precision: 0.59615  Recall: 0.57350
+
+clf = pipe_dtc.fit(features_train, labels_train)
+# clf = pipe_dtc.fit(features, labels)
+
+print(clf)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
