1b81d69425708e80fa4af6be9f4e06f23d525d1a
FrankCorrigan <franciscorrigan3@gmail.com>
2015-12-06 12:00:30 -0500

    revised final machine learning project for second submission.

--- Machine-Learning/final_project/submission/poi_id.py
+++ Machine-Learning/final_project/submission/poi_id.py
@@ -2,7 +2,7 @@
 
 import sys
 import pickle
-sys.path.append("../tools/")
+sys.path.append("../final_project/")
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
@@ -10,11 +10,7 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### the first feature must be "poi".
-features_list = ['poi', 'salary','shared_receipt_with_poi',
-                        'deferred_income','bonusexpsal',
-                        'em_ratio', 'total_navalue_count',
-                        'total_total', 'sal_navalue_count',
-                        'fromto_ratio']
+features_list = ['poi', 'exercised_stock_options', 'total_stock_value', 'bonus']
 
 ### load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
@@ -22,9 +18,6 @@ with open("final_project_dataset.pkl", "r") as data_file:
 
 ### Task 2: Remove outliers
 del data_dict['TOTAL']
-del data_dict['SKILLING JEFFREY K']
-del data_dict['BELFER ROBERT']
-del data_dict['BHATNAGAR SANJAY']
 
 ### Task 3: Create new feature(s)
 ### leverage pandas for this step
@@ -33,31 +26,33 @@ import numpy as np
 
 pdata = pd.DataFrame.from_dict(data_dict, orient='index')
 
+# pdata.loc['BELFER ROBERT']
+pdata.loc['BELFER ROBERT', 'expenses'] = 3285.0
+pdata.loc['BELFER ROBERT', 'deferral_payments'] = 0.0
+pdata.loc['BELFER ROBERT', 'deferred_income'] = -102500.0
+pdata.loc['BELFER ROBERT', 'director_fees'] = 102500.0
+pdata.loc['BELFER ROBERT', 'total_payments'] = 3285.0
+# pdata.loc['BHATNAGAR SANJAY']
+pdata.loc['BHATNAGAR SANJAY', 'expenses'] = 137864.0
+pdata.loc['BHATNAGAR SANJAY', 'total_payments'] = 137864.0
+pdata.loc['BHATNAGAR SANJAY', 'other'] = 0.0
+pdata.loc['BHATNAGAR SANJAY', 'director_fees'] = 0.0
+
 ### create NaN metadata features
 pdata = pdata.replace('NaN', np.nan, regex=True)
-temp = []
-for el in pdata['email_address'].values:
-    if type(el) == float:
-        temp.append(False)
-    else:
-        temp.append(True)
-pdata['has_email_data'] = temp
-pdata['total_navalue_count'] = pdata.isnull().sum(axis=1)
-pdata['sal_navalue_count'] = pdata[['salary', 'deferral_payments', 'long_term_incentive', 'total_payments', 'bonus', 'expenses', 'loan_advances', 'other', 'director_fees', 'deferred_income']].isnull().sum(axis=1)
+pdata['sal_navalue_count'] = pdata[['salary', 'deferral_payments', 
+								    'long_term_incentive', 'total_payments',
+								    'bonus', 'expenses', 'loan_advances',
+								    'other', 'director_fees', 'deferred_income']].isnull().sum(axis=1)
 
 ### remove text field and convert NaNs to 0
 pdata = pdata.drop('email_address', 1)
 pdata = pdata.replace(np.nan, 0, regex=True)
 
 ### create other features
-pdata['sal_ratio'] = pdata['salary'] / pdata['total_payments']
-pdata['nonsal_ratio'] = (pdata['bonus'] + pdata['long_term_incentive'] + pdata['expenses']) / pdata['total_payments']
 pdata['em_ratio'] = (pdata['shared_receipt_with_poi'] + pdata['from_this_person_to_poi'] + pdata['from_poi_to_this_person'] ) / (pdata['to_messages'] + pdata['from_messages'])
-pdata['total_total'] = pdata['total_payments'] + pdata['total_stock_value']
 pdata['from_ratio'] = pdata['from_this_person_to_poi'] / pdata['from_messages']
-pdata['sent_ratio'] = (pdata['from_poi_to_this_person'] + pdata['shared_receipt_with_poi']) / pdata['to_messages']
-pdata['fromto_ratio'] = pdata['from_ratio'] + pdata['sent_ratio']
-pdata['bonusexpsal'] = pdata['bonus'] + pdata['expenses'] + pdata['salary']
+pdata['to_ratio'] = (pdata['from_poi_to_this_person'] + pdata['shared_receipt_with_poi']) / pdata['to_messages']
 
 ### convert back to dictionary
 pdata = pdata.replace(np.nan, 0, regex=True)
@@ -93,17 +88,16 @@ labels, features = targetFeatureSplit(data)
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.decomposition import PCA
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
 
-pipe_dtc = Pipeline([('scl', MinMaxScaler()),
-                     ('pca', PCA(n_components=2, whiten=True)),
-                     ('clf', DecisionTreeClassifier(criterion='gini',
-                                                  min_samples_split=60,
-                                                  random_state=42))])
+pipe_knn = Pipeline([('scl', StandardScaler()),
+                     ('clf', KNeighborsClassifier(weights = 'uniform',
+                                                  n_neighbors = 5,
+                                                  p = 1 ))])
 
-clf = pipe_dtc.fit(features, labels)
+clf = pipe_knn.fit(features, labels)
+print(clf)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
